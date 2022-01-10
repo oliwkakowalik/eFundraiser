e5ba237bddac6625ef46e32ba82c9f5294326454
Damian Gortych <dgortych@student.agh.edu.pl>
2022-01-10 16:18:05 +0100

    add x-markdown and simple test

---
+++ Projekt/project/tests_codeception/acceptance/XMarkdownCept.php
@@ -0,0 +1,25 @@
+<?php
+
+$I = new AcceptanceTester($scenario ?? null);
+$I->wantTo('check x-markdown');
+
+$I = new AcceptanceTester($scenario ?? null);
+$I->wantTo('login with existing user');
+
+$I->amOnPage('/dashboard');
+
+$I->seeCurrentUrlEquals('/login');
+
+$I->fillField('email', 'john.doe@gmail.com');
+$I->fillField('password', 'secret');
+
+$I->click('Log in');
+
+$I->seeCurrentUrlEquals('/dashboard');
+
+$I->see('John Doe');
+$I->see("You're logged in!");
+
+$I->amOnPage('/fundraisers');
+
+$I->see("piesków",'strong');
