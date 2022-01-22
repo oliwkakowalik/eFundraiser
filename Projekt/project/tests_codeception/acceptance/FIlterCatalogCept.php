a28fa12b2cf5522b3e5e8eb90c8a932ebf160ad0
Damian Gortych <dgortych@student.agh.edu.pl>
2022-01-22 15:49:49 +0100

    Add sorting test, filter to fix

--- Projekt/project/tests_codeception/acceptance/FIlterCatalogCept.php
+++ Projekt/project/tests_codeception/acceptance/FIlterCatalogCept.php
@@ -1,4 +1,5 @@
 <?php
+use Codeception\Util\Locator;
 
 $I = new AcceptanceTester($scenario ?? null);
 $I->wantTo('Test Filtration and Sorting');
@@ -15,18 +16,16 @@ $I->see('45654.65');
 
 $I->see('Wydanie filmu');
 $I->see('54322');
-
 /*
 $I->selectOption("select", 'Animals');
 $I->click('Filter');
-
 
 $I->dontsee('Na leczenie dla Zosi');
 $I->dontsee('45654.65');
 
 $I->dontsee('Wydanie filmu');
 $I->dontsee('54322');
-*/
+
 $I->click('Show All');
 
 $I->see('Na schronisko dla kotków');
@@ -47,9 +46,28 @@ $I->dontsee('Na schronisko dla kotków');
 $I->dontsee('5450.45');
 
 $I->click('Show All');
+*/
 
+$I->click('Amount ↓');
 
+$I->see('Wynalazek',Locator::elementAt('//tbody/tr/td/a', 1));
+$I->see('Odbudowa domu po udeżeniu meteorytu',Locator::elementAt('//tbody/tr/td/a', 2));
+$I->see('Badania',Locator::elementAt('//tbody/tr/td/a', 3));
 
+$I->click('Amount ↑');
 
+$I->see('Koc',Locator::elementAt('//tbody/tr/td/a', 1));
+$I->see('Prezent dla taty',Locator::elementAt('//tbody/tr/td/a', 2));
+$I->see('Wydanie czasopisma',Locator::elementAt('//tbody/tr/td/a', 3));
 
+$I->click('End Date ↑');
 
+$I->see('Prezent dla brata',Locator::elementAt('//tbody/tr/td/a', 1));
+$I->see('Wynalazek',Locator::elementAt('//tbody/tr/td/a', 2));
+$I->see('Piłki do koszykówki',Locator::elementAt('//tbody/tr/td/a', 3));
+
+$I->click('End Date ↓');
+
+$I->see('Odbudowa domu po pożarze',Locator::elementAt('//tbody/tr/td/a', 1));
+$I->see('Wydanie filmu',Locator::elementAt('//tbody/tr/td/a', 2));
+$I->see('Na leczenie dla Zosi',Locator::elementAt('//tbody/tr/td/a', 3));
