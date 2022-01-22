0137168caec845ee4ad3386a47367c4ff29104c1
Oliwia Kowalik <okowalik@student.agh.edu.pl>
2022-01-22 11:51:36 +0100

    added some new records

--- Projekt/project/database/seeders/UserSeeder.php
+++ Projekt/project/database/seeders/UserSeeder.php
@@ -43,5 +43,95 @@ class UserSeeder extends Seeder
             'email' => 'damian.gortych@gmail.com',
             'password' => bcrypt('secret'),
         ]);
+
+        DB::table('users')->insert([
+            'name' => 'Kobe Paul',
+            'email' => 'kobe.paul@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Beatrice Travers',
+            'email' => 'beatrice.travers@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Dalton Roy',
+            'email' => 'dalton.roy@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Julia Perry',
+            'email' => 'julia.perry@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Aila Tran',
+            'email' => 'aila.tran@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Bret Bernard',
+            'email' => 'bret.bernard@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Rio Watt',
+            'email' => 'rio.watt@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Guy Hawkins',
+            'email' => 'guy.hawkins@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Korben Goddard',
+            'email' => 'korben.goddard@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Selena Gonzales',
+            'email' => 'selena.gonzales@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Zoha Monroe',
+            'email' => 'zoha.monroe@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Samson Hines',
+            'email' => 'samoson.hines@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Jarrod Denton',
+            'email' => 'jarrod.denton@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Seth Trynor',
+            'email' => 'seth.trynor@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Mylie Conway',
+            'email' => 'mylie.conway@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
     }
 }
