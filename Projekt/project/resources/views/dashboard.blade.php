9bac102df346a19995ee612489185bf7e52f21c1
Oliwia Kowalik <okowalik@student.agh.edu.pl>
2022-01-22 21:13:00 +0100

    forgot about this part:(

--- Projekt/project/resources/views/dashboard.blade.php
+++ Projekt/project/resources/views/dashboard.blade.php
@@ -87,7 +87,7 @@
                             </div>
                             <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                 <dt class="text-sm font-medium text-gray-500">
-                                    Sum of all your donations (we include even your anonymous donations, don't worry, only you can see that!):
+                                    Sum of all your donations:
                                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                                     {{ $sum_donations }}
                                 </dd>
